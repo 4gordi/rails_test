631766ca1a81bed33ea0cbdfb111a1accdee18a3
4gordi <4gordi@gmail.com>
2020-04-10 14:27:11 +0300

    Translate v1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,14 +2,11 @@ class PostsController < ApplicationController
   
   before_action :set_post, only: [ :show, :edit, :update, :destroy ]
 
-
-
   def index
    @posts = Post.all
   end
 
   def show
-
   end
   
   def new
@@ -19,31 +16,29 @@ class PostsController < ApplicationController
   def create
    @post = Post.new(post_params)
    if @post.save
-    redirect_to @post
+    redirect_to @post, success: 'Статья успешно создана'
    else
+    flash.now[:danger] = 'Статья не обновлена'
     render :new
    end
   end
 
    def edit
-
    end
    
    def update
     if @post.update_attributes(post_params)
-     redirect_to @post
+     redirect_to @post, success: 'Статья успешно обновлена'
     else
+     flash.now[:danger] = 'Статья не обновлена'
      render :edit
     end
    end
     
    def destroy
     @post.destroy
-    redirect_to posts_path
+    redirect_to posts_path, success: 'Статья успешно удалена'
    end
-
-
-
   
   private
    
